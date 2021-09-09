1a6d580a19382ad51e0747b059884ac444ec7006
emmanuelmery-git <57901989+emmanuelmery-git@users.noreply.github.com>
2021-09-09 15:58:47 +0200

    Add files via upload
    The files I created and tested locally on my computer to subscribe and publish to channel.
    The work is following instruction found in the pdf

---
+++ local poc/3_PubApiClientEvent_Order_Event__e.py
@@ -0,0 +1,161 @@
+from __future__ import print_function
+import grpc
+import requests
+import threading
+import io
+import pubsub_api_pb2 as pb2
+import pubsub_api_pb2_grpc as pb2_grpc
+import avro.schema
+import avro.io
+import datetime
+from datetime import datetime, timedelta
+import certifi
+##############################################
+import pprint as pp 
+##############################################
+import json
+with open('config.json') as json_file:
+    myConfig=json.load(json_file)
+myUserName = myConfig['username']
+myPassword = myConfig['password']
+myOrgId = myConfig['orgId']
+myDomain = myConfig['domain']
+myInstanceUrl = myConfig['instanceurl']
+myLoginUrl = myConfig['loginurl']
+myUserId = myConfig['userid']
+##############################################
+from xml.etree import cElementTree as ET
+from collections import defaultdict
+
+def etree_to_dict(t):
+    d = {t.tag: {} if t.attrib else None}
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, children):
+            for k, v in dc.items():
+                dd[k].append(v)
+        d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.items()}}
+    if t.attrib:
+        d[t.tag].update(('@' + k, v) for k, v in t.attrib.items())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+              d[t.tag]['#text'] = text
+        else:
+            d[t.tag] = text
+    return d
+##############################################
+def fetchReqStream(topic):
+    while True:
+        semaphore.acquire()
+        yield pb2.FetchRequest(
+            topic_name = topic,
+            replay_preset = pb2.ReplayPreset.LATEST,
+            num_requested = 1)
+def encode(schema, payload):
+    schema = avro.schema.Parse(schema)
+    buf = io.BytesIO(json.dumps(payload, indent=2).encode('utf-8'))
+    encoder = avro.io.BinaryEncoder(buf)
+    writer = avro.io.DatumWriter(writer_schema=schema)
+    writer.write(payload, encoder)
+    return buf.getvalue()
+def makePublishRequest(schemaid,userId,fieldValues,eventId):
+    dt = datetime.now() + timedelta(days=5)
+    payload = {
+        "CreatedDate": int(datetime.now().timestamp()),
+        "CreatedById": userId, #Your user ID        
+    }
+    for k in fieldValues.keys():
+        payload[k] = fieldValues[k]
+    
+    req = { 
+    "id": eventId, # Event ID
+    "schema_id": schemaid,
+    "payload": encode(schema, payload)
+    }
+    return [req]
+##############################################
+print('hello')
+"""
+Set a semaphore at the beginning of the program. Because of the way Python gRPC is
+designed, the program shuts down immediately if no response comes back in the
+milliseconds between calling an RPC and the end of the program. By setting a
+semaphore, you cause the client to keep running indefinitely.
+"""
+semaphore = threading.Semaphore(1)
+"""
+Create a global variable to store the replay ID.
+"""
+latest_replay_id = None
+with open(certifi.where(), 'rb') as f:
+    creds = grpc.ssl_channel_credentials(f.read())
+    with grpc.secure_channel('eventbusapi-core1.sfdc-ypmv18.svc.sfdcfc.net:7443', creds) as channel:
+        #All of the code in the rest of the tutorial will go inside
+        # this block
+    
+        url =myLoginUrl 
+    
+    
+        payload = "<soapenv:Envelope\nxmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/'\n"
+        payload = payload+"xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'\n"
+        payload = payload+"xmlns:urn='urn:partner.soap.sforce.com'><soapenv:Body>\n"
+        payload = payload+"<urn:login> "
+        payload = payload+"<urn:username><![CDATA["+myUserName+"]]></urn:username>\n"
+        payload = payload+"<urn:password><![CDATA["+myPassword+"]]></urn:password>\n"
+        payload = payload+"</urn:login></soapenv:Body></soapenv:Envelope>"
+
+        headers = {
+        "cookie": "BrowserId=-r07QQ_nEeyiUm-DzrfKRA",
+        "Content-Type": "text/xml",
+        "SOAPAction": "login",
+        "Authorization": "Basic Og=="
+        }
+        res = requests.post(url, data=payload, headers=headers, verify=False)
+        #Optionally, print the content field returned
+        #print(res.content)
+        tree = ET.XML(res.content)
+        #print('*****************')
+        xmldict = etree_to_dict(tree)
+        pp.pprint(xmldict)
+        envdict = xmldict['{http://schemas.xmlsoap.org/soap/envelope/}Envelope']
+        bodydict = envdict['{http://schemas.xmlsoap.org/soap/envelope/}Body']
+        logindict= bodydict['{urn:partner.soap.sforce.com}loginResponse']['{urn:partner.soap.sforce.com}result']
+        #
+        sessionId = logindict['{urn:partner.soap.sforce.com}sessionId']
+        instanceUrl = myInstanceUrl
+        #instanceUrl = logindict['{urn:partner.soap.sforce.com}serverUrl']
+        #instanceUrl = instanceUrl[:instanceUrl.find('/services')]
+        myDomain = myDomain 
+        #myDomain = instanceUrl[8:instanceUrl.find('.my')] # 'se' 
+        orgId = myOrgId
+        orgId = logindict['{urn:partner.soap.sforce.com}userInfo']['{urn:partner.soap.sforce.com}organizationId']
+        tenantid = "core/"+ myDomain +"/"+orgId
+        #
+        authmetadata = (('x-sfdc-api-session-token', sessionId),
+                    ('x-sfdc-instance-url', instanceUrl),
+                    ('x-sfdc-tenant-id', tenantid))
+        print(authmetadata)
+        stub = pb2_grpc.PubSubStub(channel)
+        print(stub)
+        mypubtopic = "/event/Order_Event__e"
+        schemaid = stub.GetTopic(pb2.TopicRequest(topic_name=mypubtopic),
+                    metadata=authmetadata).schema_id
+        schema = stub.GetSchema(pb2.SchemaRequest(schema_id=schemaid),
+                    metadata=authmetadata).schema_json
+        goLoop = True 
+        while goLoop:
+            print('*'*10)
+            Order_Number = input("what is the order number (text - 10) ?")
+            Has_Shipped = input ("Has Shipped value (True/False) ?")=='True' 
+            eventId = int(input("event Id (int (4)?" ))
+            publishresponse = stub.Publish(pb2.PublishRequest(topic_name=mypubtopic, events=
+            makePublishRequest(schemaid,myUserId,{"Order_Number__c": str(Order_Number),
+                        "Has_Shipped__c": Has_Shipped},str(eventId) )),
+                                                 metadata=authmetadata)
+            pp.pprint(publishresponse)
+            print('*'*10)                                            
+            goLoop = input('Do you want to send an other message (True/False) ?')=='True' 
+        
+        
---
+++ local poc/readme.md
@@ -0,0 +1,7 @@
+chaneg the config.json file to add password to it.
+
+Start the python in the following order
+
+1_SubApiClientEvent_Order_Event__e.py
+2_SubApiClientObject_User.py
+3_PubApiClientEvent_Order_Event__e.py
